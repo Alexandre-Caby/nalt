e1ae0e46323b313e1189113d0b0295a64dbfab8c
Alexandre-Caby <alexandre.caby@ig2i.centralelille.fr (GitHub)>
2025-06-16 17:09:13 +0200

    up

---
+++ api-management/bdd_config.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'money_api_rest',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// connection.connect(err => {
+//     if (err) {
+//         console.error('Erreur Connexion :', err);
+//         return;
+//     }
+//     console.log('Connecté à la base de données money_api');
+// });
+
+module.exports = connection;
